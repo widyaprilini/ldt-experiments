8b941f82672bd662275900a0178efe704f6e9e6c
widyaprilini <widyaprilini@gmail.com>
2026-04-19 18:12:14 +0700

    reset deployment setup backend

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
 app.use("/api/ldt", ldtRoutes);
 
 module.exports = app;
