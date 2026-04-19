fa69481964c353393e30a6717d56b4e4505faca1
widyaprilini <widyaprilini@gmail.com>
2026-04-19 18:53:53 +0700

    fix deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,5 @@
 const app = require("../app");
 
-module.exports = app;
+module.exports = (req, res) => {
+  return app(req, res);
+};
--- backend/routes/ldt/ldt.routes.js
+++ backend/routes/ldt/ldt.routes.js
@@ -5,4 +5,10 @@ const { saveLdtResponseHandler } = require("./ldt.handler");
 
 router.post("/response", saveLdtResponseHandler);
 
+router.get("/health-check", (req, res) => {
+  res.status(200).json({
+      message: "Backend OK"
+    });
+});
+
 module.exports = router;
