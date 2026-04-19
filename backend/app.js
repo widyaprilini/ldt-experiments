afebec777301d4c3b66ce45f71d3c648e2b46eed
widyaprilini <widyaprilini@gmail.com>
2026-04-19 17:55:40 +0700

    fix deploymnet

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 app.use("/api/ldt", ldtRoutes);
